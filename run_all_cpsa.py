84cb95e582a80c5ba7b7625a40823379492e63e4
Rebecca-L28 <lee.rebecca1@ufl.edu>
2025-11-10 09:05:16 -0500

    Python file to run all CPSA commands

---
+++ run_all_cpsa.py
@@ -0,0 +1,34 @@
+import subprocess
+import os
+
+files = [
+    "bsp_integrity",
+    "bsp_pib_auth",
+    "bsp_replay_flawed",
+    "bsp_replay_unique",
+    "sdls_auth",
+    "sdls_conf",
+    "sdls_integrity",
+    "sdls_replay"
+]
+
+def run_cpsa_pipeline(file):
+    scm = f"{file}.scm"
+    analysis = f"{file}_analysis.txt"
+    shapes = f"{file}_shapes.txt"
+    xhtml = f"{file}_models.xhtml"
+
+    print(f"Running CPSA analysis for {scm}...")
+
+    subprocess.run(["cpsa4", "-o", analysis, scm])
+    subprocess.run(["cpsa4shapes", "-o", shapes, analysis])
+    subprocess.run(["cpsa4graph", "-o", xhtml, shapes])
+
+    print(f"Done: {xhtml}")
+    print("-" * 40)
+
+
+for file in files:
+    run_cpsa_pipeline(file)
+
+print("All CPSA analyses complete.")
